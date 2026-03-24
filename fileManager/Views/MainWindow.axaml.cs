f88806f18f06327607f44d3d533665c4a6354e63
isHardCoded <ishardcoded@gmail.com>
2026-03-24 17:22:17 +0300

    Добавьте файлы проекта.

---
+++ fileManager/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace fileManager.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
