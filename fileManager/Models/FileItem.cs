238ea801ab58ea3dd331aed1d41af2223f5f7eb6
isHardCoded <ishardcoded@gmail.com>
2026-03-24 17:31:27 +0300

    feat: добавили модель для файла

---
+++ fileManager/Models/FileItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace fileManager.Models
+{
+    public class FileItem
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+        public long Size { get; set; }
+        public DateTime Created { get; set; }
+        public bool IsDirectory { get; set; }
+    }
+}
